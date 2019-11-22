b67988a656d6baae552d4629cc9b928a91ce92aa
Johnj11 <54954715+Johnj11@users.noreply.github.com>
2019-11-22 11:08:26 -0800

    Create form.php

---
+++ form.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+		<style>
+		</style>
+	</head>
+	<body>
+		<form action="runPost.php" method="post">
+		Name: <input type="text" name="name"<br>
+		E-Mail: <input type="text" name="email"<br>
+		<input type="submit" value="Post">
+		</form>
+		<form action="runGet.php" method="get">
+		Name: <input type="text" name="name"<br>
+		E-Mail: <input type="text" name="email"<br>
+		<input type="submit" value="Get">
+		</form>
+	</body>
+</html>
