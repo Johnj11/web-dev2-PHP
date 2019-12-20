503dbb05f6a1f1b81d0a5325a22d7dbb75a2b670
Johnj11 <54954715+Johnj11@users.noreply.github.com>
2019-12-20 11:10:50 -0800

    Update imageInputTest.php

--- rotary/imageInputTest.php
+++ rotary/imageInputTest.php
@@ -5,12 +5,23 @@
 		</style>
 	</head>
 	<body>
-		
+		<img src="" alt="" id="EmptyImage">
 		<input type="file" name="fileupload" value="fileupload" id="fileupload"> 
 		<label for="fileupload"> Select a file to upload</label> 
-		<input type="submit" value="submit"> 
+		<input type="submit" value="submit" id = "subBtn"> 
+		<?php
+			echo("PicInnerHTML")
 		
+		?>
 		<script>
+		var PicInnerHTML = document.getElementById("fileuplod").innerHTML;
+		
+		document.getElementById("subBtn").addEventlistener("click", picInput);
+		
+		function picInput(){
+			picInnerHTML;
+		}
+		
 		</script>
 	</body>
 </html>
